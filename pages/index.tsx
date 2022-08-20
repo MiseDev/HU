32f2264853ac3ea945c200b39ed443b213a67760
MiseDev <huylemise@gmail.com>
2022-08-20 15:44:59 -0230

    MiseDev

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
 import styles from '../styles/Home.module.css'
-import { motion } from 'framer-motion'
+import { motion } from '../framer-motion'
 import Script from 'next/script'
 import { useRouter } from 'next/router'
 const Home: NextPage = () => {
